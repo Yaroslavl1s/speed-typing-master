d6d9240904f59c13b8bd957e53e6777e71cf9138
Yaroslavl1s <lisinskijaroslav@gmail.com>
2023-09-14 19:46:52 +0300

    fdg

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import {BrowserRouter as Router,} from 'react-router-dom';
 import GeneratedWords from "./components/GeneratedWords";
 import RestartButton from "./components/RestartButton";
 import Results from "./components/Results";
